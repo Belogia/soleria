8966a6a6b42fcd46cd14fe156320507fd6a9f5b8
gzordrai <gzordrai@users.noreply.github.com>
2023-12-27 17:49:23 +0100

    Add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+function Login () {
+    return (
+        <div>
+            <h1>Login</h1>
+            <a href="http://localhost:4000/auth/discord/login">Connection to discord</a>
+        </div>
+    );
+}
+
+export default Login;
