89a664a4e10a169236d84b5a404efa95b42da236
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-03-29 23:30:25 +0100

    Fix indentation in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Login from "./pages/Login";
 import "./styles/App.css";
 
 function App() {
-  const token: string | undefined = Cookies.get("token");
+    const token: string | undefined = Cookies.get("token");
 
-  return token ? <Dashboard /> : <Login />;
+    return token ? <Dashboard /> : <Login />;
 }
 
 export default App;
