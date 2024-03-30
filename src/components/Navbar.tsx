7e5d227ff1f4730c319e879370ebf89b616fbedb
gzordrai <gzordrai@users.noreply.github.com>
2024-03-30 19:49:11 +0100

    Add dynamic bot list to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,39 @@
+import { useCallback, useEffect, useState } from "react";
+import { INavbarProps } from "../types";
+import axios from "axios";
 import "../styles/navbar.css";
 
-function Navbar() {
-  return (
-    <div className="navbar">
-      {/* <button onClick={onSignOutClick} className="sign-out-button">
-        Sign Out
-      </button> */}
-      <ul className="nav-links">
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3</a></li>
-      </ul>
-    </div>
-  );
+function Navbar({ selectedGuild, onBotClick }: Readonly<INavbarProps>) {
+    const [bots, setBots] = useState([]);
+
+    const getBots = useCallback(async () => {
+        const response = await axios.get(`http://localhost:3000/api/guild/${selectedGuild}/bots`, {
+            withCredentials: true,
+        });
+    
+        setBots(response.data.bots);
+    }, [selectedGuild]);
+    
+    useEffect(() => {
+        if (selectedGuild !== null)
+            getBots();
+    }, [selectedGuild, getBots]);
+
+    return (
+        <div className="navbar">
+            <ul className="nav-links">
+                {
+                    bots.map((bot: { id: string, name: string }) => (
+                        <li key={bot.id} className="nav-link">
+                            <button className="nav-button" onClick={() => onBotClick(bot.id)}>
+                                {bot.name}
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default Navbar;
