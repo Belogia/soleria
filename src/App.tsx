b6569a086283f5a2af6782e94cb304e290119dbc
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-04-05 15:32:05 +0200

    Update App.css import to use Bootstrap CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Cookies from "js-cookie"
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     const token: string | undefined = Cookies.get("token");
