fd5f7984a0a6428c031dee598c4b85feb2e28521
gzordrai <gzordrai@users.noreply.github.com>
2024-04-30 21:47:17 +0200

    Rework login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,25 @@
+import { Button } from "reactstrap";
 import LoginIcon from "../components/LoginIcon";
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-            <a href="http://localhost:3000/auth/discord/login">
-                <LoginIcon />
-            </a>
+        <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh", backgroundColor: "#2f3136" }}>
+          <Button
+            className="d-flex align-items-center py-2 px-4 rounded transition duration-300"
+            href="https://api.belogia.fr/auth/discord"
+            style={{
+              backgroundColor: "#5865F2",
+              color: "white",
+              transition: "all 0.3s ease",
+            }}
+            onMouseOver={(e) => e.currentTarget.style.backgroundColor = "rgba(88, 101, 242, 0.8)"}
+            onMouseOut={(e) => e.currentTarget.style.backgroundColor = "#5865F2"}
+          >
+            <LoginIcon />
+            <span className="text-sm">Sign in with Discord</span>
+          </Button>
         </div>
-    );
+      );
 }
 
 export default Login;
